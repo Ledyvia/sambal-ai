faf2559d2bb02130e62d3971a65f8fa6c4b9cfa5
Ledyvia <audiz.coranov@gmail.com>
2026-07-26 01:07:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -646,6 +646,5 @@ def blog_detail(slug):
         return "Artikel tidak ditemukan", 404
     return render_template("blog_detail.html", post=post)
 
-
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
